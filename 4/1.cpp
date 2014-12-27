e769df87c56f7b4de44a2ea4b6cc9c9c95489e1f
Deepankar <kamalneet@gmail.com>
2014-12-28 01:32:11 +0530

    110401 Vito's Family

--- 4/1.cpp
+++ 4/1.cpp
@@ -51,8 +51,17 @@ void pv(const vector<T> &v){
 class Solver
 {
    public:
-   void solve()
+   int solve(const VI &l)
    {
+      int mn = INT_MAX;
+      for(int i = 1; i < 30000;i++){
+         int s = 0;
+         FOR0(j,l.size()){
+            s += abs(i-l[j]);
+         }
+         mn = min(mn,s);
+      }
+      return mn;
    }
 };
 
@@ -62,18 +71,15 @@ int main(int argc, char **argv)
    int tt;
    cin >> tt;
    FOR0(t,tt){
-   }
-   int m,n;
-   while(cin.good()){
-      //string line;
-      //getline(cin, line);
-
-      cin >> m >> n;
-      if(!cin.good()){
-         return 0;
+      int n;
+      cin >> n;
+      VI l;
+      FOR0(i,n){
+         int x;
+         cin >> x;
+         l.push_back(x);
       }
-      //cout << line << endl;
-      //slv.solve();
+      cout << slv.solve(l) << endl;
    }
    return 0;
 }
